4ea30e84ec224934411c487b3e6ac0fc5b4b4b42
何建启 <jian-qi.he@utsoft.cn>
2016-08-24 15:14:46 +0800

    二分法

--- FistGitDemo/FistGitDemo/ViewController.swift
+++ FistGitDemo/FistGitDemo/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         // 大师法撒旦法师法
+        // 发发发啥打法上发送到
+        // fafafdasfdsaFsdf撒旦
     }
 
     override func didReceiveMemoryWarning() {
